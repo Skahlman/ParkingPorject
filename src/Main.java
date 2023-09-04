b671f9c441aa0ed12dc1ea702f5b0f562fd57f11
eskold <emesko20@student.hh.se>
2023-09-04 14:03:03 +0200

    Changess

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("now i'm trying another push");
-        System.out.println("hej på dig");
+        System.out.println("ok");
     }
 }
