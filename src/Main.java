53f02d2a365b72922ea95d38dded5de7830fc860
eskold <emesko20@student.hh.se>
2023-09-04 14:00:33 +0200

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("now i'm trying another push");
+        System.out.println("hej på dig");
     }
 }
